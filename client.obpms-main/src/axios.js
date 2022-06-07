b08dc83ccfe31c2e7b1e0260d7f9d73c7b958c0c
Navneet Maurya <mauryanavis@gmail.com>
2022-06-07 14:08:56 +0530

    .

--- client.obpms-main/src/axios.js
+++ client.obpms-main/src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://54.190.5.246:5000'
-    baseURL: 'http://localhost:5000'
-    // baseURL: 'https://backend-obpms.herokuapp.com'
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://api.navimaurya.in'
 });
 
 
